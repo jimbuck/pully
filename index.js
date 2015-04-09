8df17f834260ceebe2704b7ec52faa5e9644197c
Jim Buck <jim@jimmyboh.com>
2015-04-09 17:03:44 -0400

    Minor cleanup.

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-console.info('This module is a work in progress. Module documentation will be available in the future, but in the meantime check out ./bin/pully.js for an example implementation.');
+
 module.exports = require('./lib/main.js');
