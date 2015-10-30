c53e26c450804d05f226737ab43fd05e103f4694
Jim Buck <jim@jimmyboh.com>
2015-10-30 09:00:56 -0400

    Boosted core test coverage. Cleaned up gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 var connect = require('gulp-connect');
 
+// Setup the paths for the various folders/files.
 var paths = {
   all: './*',
   root: './',
@@ -10,52 +11,61 @@ var paths = {
   data: './data/**/*.*',
   spec: './spec/**/*.*',
   coverage: './coverage/**/*.*',
-  coverageReport:'./coverage/lcov-report'
+  coverageReport: './coverage/lcov-report'
 };
 
+// Set the default task to the dev task.
 gulp.task('default', ['dev']);
 
+// Runs the ESLinter with reporting only.
 gulp.task('lint', execTask('eslint -c ./.eslintrc ./'));
 
+// Runs the ESLinter with reporting and auto fix.
 gulp.task('lint:fix', execTask('eslint -c ./.eslintrc --fix ./'));
 
-gulp.task('test', ['lint:fix'], execTask('istanbul cover -x \"*.spec.js\" --print detail ./node_modules/jasmine-node/bin/jasmine-node spec'));
+// ESLints the code, autofixes and runs the tests and gathers code coverage on source files (used on demand).
+gulp.task('test', ['lint'], execTask('istanbul cover -x \"*.spec.js\" --print detail ./node_modules/jasmine-node/bin/jasmine-node spec'));
 
+// ESLints the code and runs the tests and gathers code coverage on all files (used during development).
 gulp.task('test:all', ['lint'], execTask('istanbul cover --print detail ./node_modules/jasmine-node/bin/jasmine-node spec'));
 
+// ESLints the code, runs the tests, and reports the coverage results to codeclimate.com
 gulp.task('test:ci', ['test'], execTask('codeclimate-test-reporter < ./coverage/lcov.info'));
 
-gulp.task('coverage-server', function() {
+// Starts a webserver for the code coverage report.
+gulp.task('coverage-server', function () {
   connect.server({
     root: paths.coverageReport,
     livereload: true
   });
 });
 
+// Re-runs all the tests and live-reloads the webserver.
 gulp.task('refresh', ['test:all'], function () {
   try {
     gulp.src(__filename)
-    .pipe(connect.reload());
-  } catch (e) {
+      .pipe(connect.reload());
+  }
+  catch (e) {
     console.log(e);
   }
 });
 
-gulp.task('dev', ['coverage-server'], function () {
+// Starts the coverage server and then watches for changes
+gulp.task('dev', ['coverage-server', 'test:all'], function () {
   gulp.watch([paths.index, paths.lib, paths.spec, paths.data], ['refresh']);
 });
 
+// Creates a gulp task that runs a command and outputs the results, passing the error into the gulp callback.
 function execTask(command) {
   return function (cb) {
     exec(command, function (err, stdout, stderr) {
       if (stdout) {
         console.log(stdout);
       }
-
       if (stderr) {
         console.log(stderr);
       }
-
       cb(err);
     });
   };
--- spec/core.spec.js
+++ spec/core.spec.js
@@ -1,16 +1,18 @@
+var proxy = require('proxyquire').noCallThru();
+
+function Downloader() { }
+Downloader['@noCallThru'] = true;
+
+var pully = proxy('../lib/core', {
+  '../lib/downloader': Downloader
+});
+var ERROR_CODES = require('../data/error-codes');
+
 describe('Core', function () {
 
-  var pully = require('../index');
-  var Downloader;
-
   beforeEach(function () {
-
-    Downloader = function () { };
-
-    var filePath = 'temp/test.test';
-
     Downloader.prototype.download = function (options, callback) {
-      callback(null, options, filePath);
+      callback(null, options, 'temp/test.test');
     };
   });
 
@@ -18,12 +20,13 @@ describe('Core', function () {
     expect(typeof pully).toBe('function');
   });
 
+
   it('should require a hash of options', function (done) {
 
     pully(null, function (err) {
       expect(err).toBeDefined();
       done();
-    }, Downloader);
+    });
   });
 
   it('should require a url', function (done) {
@@ -32,7 +35,23 @@ describe('Core', function () {
     }, function (err) {
       expect(err).toBeDefined();
       done();
-    }, Downloader);
+    });
+  });
+  
+  it('should have a default callback that throws on err', function () {
+    var errorMessage = 'Some Known Error';
+
+    Downloader.prototype.download = function (options, callback) {
+      callback(new Error(errorMessage), options);
+    };
+    
+    var options = {
+      url: 'https://www.youtube.com/watch?v=1F5Ad0dhScQ'
+    };
+    
+    expect(function () {
+      pully(options);
+    }).toThrow(errorMessage);
   });
 
   it('should only accept YouTube URLs', function (done) {
@@ -42,7 +61,7 @@ describe('Core', function () {
     }, function (err) {
       expect(err).toBeDefined();
       done();
-    }, Downloader);
+    });
   });
 
   it('should default to `hd` as the preset', function (done) {
@@ -51,12 +70,11 @@ describe('Core', function () {
       url: 'https://www.youtube.com/watch?v=1F5Ad0dhScQ'
     };
 
-    Downloader.prototype.download = function (options) {
-      expect(options.preset).toBe('hd');
+    pully(options, function (err, opts) {
+      expect(err).toBeFalsy();
+      expect(opts.preset).toBe('hd');
       done();
-    };
-
-    pully(options, null, Downloader);
+    });
   });
 
   it('should execute a callback with filepath when download is complete', function (done) {
@@ -67,7 +85,25 @@ describe('Core', function () {
     }, function (err) {
       expect(err).toBeFalsy();
       done();
-    }, Downloader);
+    });
+  });
+  
+  it('should catch any unexpected errors', function () {
+    var errorMessage = 'Some Unexpected Error';
+
+    Downloader.prototype.download = function () {
+      throw new Error(errorMessage);
+    };
+    
+    var options = {
+      url: 'https://www.youtube.com/watch?v=1F5Ad0dhScQ'
+    };
+    
+    pully(options, function (err) {
+      expect(err).toBeDefined();
+      expect(err.code).toBe(ERROR_CODES.UNKNOWN);
+      expect(err.message).toBe(errorMessage);
+    });
   });
 
 });
