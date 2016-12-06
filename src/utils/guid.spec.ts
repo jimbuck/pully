abaefcfc8f4e86eb28372233cb2725808e2f9875
Jim Buck <jim@jimmyboh.com>
2016-12-06 07:33:42 -0500

    Updated guids to v4 and fixed tests.

--- src/utils/guid.spec.ts
+++ src/utils/guid.spec.ts
@@ -6,6 +6,11 @@ test(`guid produces a unique string`, t => {
   let guids = new Set<string>();
 
   for (let i = 0; i < 10000; i++) {
-    t.false(guids.has(guid()));
+    const id = guid();
+    const isDup = guids.has(id);
+    guids.add(id);
+
+    t.false(isDup, 'Guid was a duplicate!');
+    t.is(id[14], '4'); // UUIDv4
   }
 });
--- src/utils/guid.ts
+++ src/utils/guid.ts
@@ -1,9 +1,13 @@
 
 
 export function guid() {
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  return s4() + s4() + '-' + s4() + '-' + s4(true) + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
-function s4() {
-  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+function s4(v4: boolean = false) {
+  if (v4) {
+    return '4' + Math.floor((1 + Math.random()) * 0x1000).toString(16).substring(1);
+  } else {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
 }
