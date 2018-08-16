e6fe52523a672330686a1c8a5464ce9341607aaf
Jim Buck <jim@jimmyboh.com>
2018-08-16 10:10:47 -0400

    Removed missing file (according to TravisCI).

--- travis-setup.sh
+++ travis-setup.sh
@@ -9,7 +9,7 @@ wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz
 tar xf ffmpeg-git-64bit-static.tar.xz
 
 mkdir -p $HOME/bin
-cp ffmpeg-git-*-static/{ffmpeg,ffprobe,ffserver} $HOME/bin
+cp ffmpeg-git-*-static/{ffmpeg,ffprobe} $HOME/bin
 cp ffmpeg-git-*-static/{ffmpeg,ffprobe} $(pwd)
 
 export PATH=$(pwd)/bin:$PATH
